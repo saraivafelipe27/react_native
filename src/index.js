48c7fdb9754e8e9214fc59f7e5b765e7fcc3edb2
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-03-31 17:54:41 -0300

    chore: configuração do react-navigation

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#292c2e',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#FFF',
-  },
-});
-
-console.tron.warn('Hello World!');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native</Text>
-    </View>
-  );
+  return <Routes />;
 }
